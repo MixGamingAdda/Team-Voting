47f1951c863b23e8fa8e6c0efdeec30c31b7bd2c
MixGamingAdda <amanasrj4545@gmail.com>
2022-12-10 17:01:14 +0530

    added SDK

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyDdyNtRdpImzp9B3hMwQ5xIB1qLFtMbkUQ",
+  authDomain: "voting-app-f1419.firebaseapp.com",
+  databaseURL: "https://voting-app-f1419-default-rtdb.firebaseio.com",
+  projectId: "voting-app-f1419",
+  storageBucket: "voting-app-f1419.appspot.com",
+  messagingSenderId: "325062819752",
+  appId: "1:325062819752:web:5cf63fe8ad47946fa4e79d",
+  measurementId: "G-ETK9DEYMT8"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
